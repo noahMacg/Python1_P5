9d1ce35ef135e8ef468cea53f125fa5db77c49ed
macgillivraynoah@gmail.com <macgillivraynoah@gmail.com>
2025-02-20 19:57:07 -0700

    Header and started game play

--- MacGillivrayP5.py
+++ MacGillivrayP5.py
@@ -1,4 +1,19 @@
 # MacGillivrayP5
 # Programmer: Noah MacGillivray
 # EMail: nmacgillivray@cnm.edu
-# Purpose: Create rock, paper and scissors game
+# Purpose: Create rock, paper, scissors game
+
+print("------Welcome to (block) ROCK (in'), PAPER, SCISSORS (beats)------")
+print("Be prepared to Dig Your Own Hole")
+print("The best rock paper scissors game since 97'\n")
+
+import random
+
+user_input = ""
+game_results = []
+round_start = 'Enter "rock" "paper or "scissors"'
+
+while user_input != "no":
+    user_input = input(round_start)
+
+    break
